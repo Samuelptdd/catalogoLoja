3968ba43b28522e877f4c7adaa694b3e1bc55488
Samuelptdd <samuelpaivaramos@gmail.com>
2025-10-07 01:15:18 -0300

    :tada: primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
